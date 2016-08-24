4ca93d4856b755065ed3560c843d36045734f0fe
dg <dg@shadowrobot.com>
2016-08-24 15:48:45 +0100

    typo

--- sr_blockly_blocks/generators/python/scripts/warehouse_save.py
+++ sr_blockly_blocks/generators/python/scripts/warehouse_save.py
@@ -3,7 +3,7 @@ from moveit_msgs.srv import SaveRobotStateToWarehouse as SaveState
 from sensor_msgs.msg import JointState
 from moveit_msgs.msg import RobotState
 from sr_utilities.hand_finder import HandFinder
-from sr_robot_commander.sr_robot_state_saver import SrGraspSaverUnsafe
+from sr_robot_commander.sr_robot_state_saver import SrStateSaverUnsafe
 
 if "__main__" == __name__:
 
@@ -14,5 +14,5 @@ if "__main__" == __name__:
     else:
         which = 'hand'
 
-    gs = SrGraspSaverUnsafe(pose_name, which)
+    gs = SrStateSaverUnsafe(pose_name, which)
     gs.spin()
