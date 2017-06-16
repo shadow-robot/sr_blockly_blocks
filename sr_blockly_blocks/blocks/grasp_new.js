201c1b4156ab7e59aedd130abb7ae69561c1d12a
anastasis <anzompas@gmail.com>
2017-06-16 18:28:59 +0100

    Rename grasp.js to grasp_new.js

---
+++ sr_blockly_blocks/blocks/grasp_new.js
@@ -0,0 +1,18 @@
+Blockly.Blocks['grasp'] = {
+  init: function() {
+    this.appendValueInput("grasp_id")
+        .setCheck("String")
+        .appendField("grasp id");
+    this.appendValueInput("max torque")
+        .setCheck("Number")
+        .appendField("max torque");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["pregrasp","pre_grasp"], ["grasp","grasp"]]), "grasp_menu");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
