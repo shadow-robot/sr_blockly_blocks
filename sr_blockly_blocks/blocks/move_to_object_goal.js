3d5aa66f0620ea7ef528b13a2ecf4cf265fddbcf
anastasis <anzompas@gmail.com>
2017-06-16 18:43:59 +0100

    Update move_to_object_goal.js

--- sr_blockly_blocks/blocks/move_to_object_goal.js
+++ sr_blockly_blocks/blocks/move_to_object_goal.js
@@ -3,6 +3,9 @@ Blockly.Blocks['move_to_object_goal'] = {
     this.appendDummyInput()
         .appendField("Move ")
         .appendField(new Blockly.FieldDropdown([["above object","object"], ["to goal","goal"]]), "move_drop_down");
+    this.appendValueInput("object")
+        .setCheck("String")
+        .appendField("which object to grasp");
     this.setPreviousStatement(true, "Array");
     this.setNextStatement(true, null);
     this.setColour(230);
