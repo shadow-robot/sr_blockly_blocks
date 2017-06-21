238294b44d7a9900fc6a963190e176cc4059ae6d
James Andrews <james@shadowrobot.com>
2017-06-21 15:54:53 +0000

    Added tf broadcast of object frames

--- sr_blockly_blocks/generators/python/scripts/recognizer_client.py
+++ sr_blockly_blocks/generators/python/scripts/recognizer_client.py
@@ -1,11 +1,17 @@
 # from __future__ import print_function
-# import rospy
 
 import actionlib
 
-
 # goal message and the result message.
 import sr_recognizer.msg
+
+
+
+import rospy
+import tf
+import tf2_ros
+import geometry_msgs.msg
+
 
 
 def recognizer_client():
@@ -32,15 +38,47 @@ def recognizer_client():
 
 if __name__ == '__main__':
     try:
+        # rate = rospy.Rate(10.0)
+        # while not rospy.is_shutdown():
+
         #     Initializes a rospy node so that the SimpleActionClient can
         #     publish and subscribe over ROS.
         #     rospy.init_node('recognizer_client_py') : not needed, because done by blockly
         result = recognizer_client()
+        # print result
 
         result_names = list()
 
         for i in xrange(0, len(result.ids)):
             result_names.append(result.ids[i].data)
+            # print result.ids[i]
+
+        # rospy.init_node('recognizer_client_py')
+        tf_br = tf2_ros.TransformBroadcaster()
+        t_msg = geometry_msgs.msg.TransformStamped()
+
+        # trans_pub = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.TransformStamped,
+        #                              queue_size=1)
+
+        for i in xrange(0, len(result.ids)):
+
+            parent_frame = "camera_depth_optical_frame"
+            # parent_frame = "camera_rgb_optical_frame"
+
+            name = result.ids[i]
+            transform = result.transforms[i]
+
+            t_msg.header.stamp = rospy.Time.now()
+            t_msg.header.frame_id = parent_frame
+            t_msg.child_frame_id = str(name)
+
+            t_msg.transform.translation = transform.translation
+
+            t_msg.transform.rotation = transform.rotation
+
+            tf_br.sendTransform(t_msg)
+
+            # rate.sleep()
 
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
