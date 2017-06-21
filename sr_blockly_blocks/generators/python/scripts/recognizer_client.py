08b5e3385c24f27bf0d267c382bcdaf21854fb5b
James Andrews <james@shadowrobot.com>
2017-06-21 16:06:54 +0000

    Formatting

--- sr_blockly_blocks/generators/python/scripts/recognizer_client.py
+++ sr_blockly_blocks/generators/python/scripts/recognizer_client.py
@@ -5,14 +5,9 @@ import actionlib
 # goal message and the result message.
 import sr_recognizer.msg
 
-
-
 import rospy
-import tf
 import tf2_ros
 import geometry_msgs.msg
-
-
 
 def recognizer_client():
     # Creates the SimpleActionClient, passing the type of the action
@@ -38,32 +33,22 @@ def recognizer_client():
 
 if __name__ == '__main__':
     try:
-        # rate = rospy.Rate(10.0)
-        # while not rospy.is_shutdown():
-
         #     Initializes a rospy node so that the SimpleActionClient can
         #     publish and subscribe over ROS.
         #     rospy.init_node('recognizer_client_py') : not needed, because done by blockly
         result = recognizer_client()
-        # print result
 
         result_names = list()
 
         for i in xrange(0, len(result.ids)):
             result_names.append(result.ids[i].data)
-            # print result.ids[i]
 
-        # rospy.init_node('recognizer_client_py')
         tf_br = tf2_ros.TransformBroadcaster()
         t_msg = geometry_msgs.msg.TransformStamped()
-
-        # trans_pub = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.TransformStamped,
-        #                              queue_size=1)
 
         for i in xrange(0, len(result.ids)):
 
             parent_frame = "camera_depth_optical_frame"
-            # parent_frame = "camera_rgb_optical_frame"
 
             name = result.ids[i]
             transform = result.transforms[i]
@@ -77,8 +62,6 @@ if __name__ == '__main__':
             t_msg.transform.rotation = transform.rotation
 
             tf_br.sendTransform(t_msg)
-
-            # rate.sleep()
 
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
